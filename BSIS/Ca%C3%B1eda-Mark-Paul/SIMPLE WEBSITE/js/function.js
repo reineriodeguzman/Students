c83233e6296068abe0888572eb953489fb9b5257
Mark Paul Cañeda <markpaulcaneda1997@gmail.com>
2016-10-13 11:48:39 +0800

    Add files via upload

---
+++ BSIS/Ca%C3%B1eda-Mark-Paul/SIMPLE WEBSITE/js/function.js
@@ -0,0 +1,25 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+  }
+}
