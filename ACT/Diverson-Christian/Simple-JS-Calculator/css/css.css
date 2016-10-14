ca882ca186a2ca17c60d0b5b7b574c1d50967fb0
⊂hristian ⊃iverson <diversonchristian@gmail.com>
2016-10-14 23:37:00 +0800

    Create css.css

---
+++ ACT/Diverson-Christian/Simple-JS-Calculator/css/css.css
@@ -0,0 +1,125 @@
+body { 
+ 	width: 99%;
+    height: auto;
+    background-image: url('../desk.jpg');
+    background-size: cover;
+}
+
+#calculator {
+	width: 310px;
+	height: 360px;
+	border: 1px solid gray;
+	margin-top: 210px;
+	margin-left: auto;
+	margin-right: auto;
+	margin-bottom: 50px;
+	border-radius: 10px;
+	text-align: center;
+	background-image: url('../calcbg.jpg');
+	box-shadow: 0px 0px 5px #959595;
+}
+
+#display {
+	width: 285px;
+	height: 50px;
+	margin-top: 15px;
+	margin-bottom: 15px;
+	text-align: right;
+	font-size: 32px;
+	color: #b7b7b7;
+	border: 0px;
+	letter-spacing: 2.5px;
+	background: #252525;
+}
+
+#keys {
+	height: 45px;
+	width: 65px;
+	margin-left: 5px;
+	margin-bottom: 10px;
+	cursor: pointer;
+	border-radius: 3px;
+	border: 1px solid;
+	border-color: #555555;
+	color: #252525;
+}
+
+#clear {
+	height: 45px;
+	width: 140px;
+	margin-left: 5px;
+	margin-bottom: 10px;
+	cursor: pointer;
+	border-radius: 3px;
+	border: 1px solid;
+	border-color: #555555;
+	color: #252525;
+}
+
+#zero {
+	height: 45px;
+	width: 140px;
+	margin-left: 5px;
+	margin-bottom: 10px;
+	cursor: pointer;
+	border-radius: 3px;
+	border: 1px solid;
+	border-color: #555555;
+	color: #252525;
+}
+
+#operator {
+	height: 45px;
+	width: 65px;
+	margin-left: 5px;
+	margin-bottom: 10px;
+	cursor: pointer;
+	border-radius: 3px;
+	border: 1px solid;
+	border-color: #555555;
+	color: #ffffff;
+	background-color: #f7941d;
+}
+
+
+#keys:hover {
+	background: #b7b7b7;
+	border: 1px solid #252525;
+	border-radius: 3px;
+	color: #252525;
+}
+
+#clear:hover {
+	background: #b7b7b7;
+	border: 1px solid #252525;
+	border-radius: 3px;
+	color: #252525;
+}
+
+#zero:hover {
+	background: #b7b7b7;
+	border: 1px solid #252525;
+	border-radius: 3px;
+	color: #252525;
+}
+
+#operator:hover {
+	background: #f7941d;
+	border: 1px solid #555555;
+	border-radius: 3px;
+	color: #555555;
+}
+
+p.bcalc {
+	text-align: center;
+	font-weight: bold;
+	font-size: 24px;
+	color: #252525;
+	letter-spacing: 2px;
+}
+
+p.by {
+	text-align: center;
+	color: #252525;
+	letter-spacing: 2px;
+}
