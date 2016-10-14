cbfd77516178a99039251d9a0931c0f0392aa040
Allyson Mae Tubtub <allysonmae17@gmail.com>
2016-10-14 19:03:11 +0800

    Create calcjs.js

---
+++ BSIS/Tubtub-Allyson-Mae/FinalCalculator/calcjs.js
@@ -0,0 +1,17 @@
+function c(val){
+	document.getElementById("display").value=val;
+}
+
+function v(val){
+	document.getElementById("display").value+=val;
+}
+
+function e(){
+	try{
+		c(eval(document.getElementById("display").value))
+	}
+
+	catch(e){
+		c('Error')
+	}
+}
